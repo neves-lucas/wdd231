f3e7f994965cc46a7365ccf255a4726b1582219a
neves-lucas <lrocha1@byupathway.edu>
2025-02-14 08:19:37 -0300

    Add form styles, navigation functionality, and contact page with form submission

---
+++ project/scripts/script.js
@@ -0,0 +1,27 @@
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+// Dark mode functionality
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.getElementById('main');
+
+// Check and apply saved theme from local storage
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+	body.classList.add(currentTheme);
+}
+
+// Toggle dark mode and save preference
+toggleButton.addEventListener('click', () => {
+	body.classList.toggle('dark-mode');
+	if (body.classList.contains('dark-mode')) {
+		localStorage.setItem('theme', 'dark-mode');
+	} else {
+		localStorage.removeItem('theme');
+	}
+});
